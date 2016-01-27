aeb9eea152f1de4f900848b010aa5bce8d312507
Jason Woods <jwoo@Jasons-Mac-Pro.local>
2016-01-26 19:06:58 -0700

    fix for render issue on nested link

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ serve = require('koa-static');
 var route= require('koa-route');
 app.use(require('koa-connect-history-api-fallback')({
   verbose:true,
-  
+
 }))
-app.use(serve(__dirname + '/dist'))
+app.use(serve('dist'))
 
 
 app.listen(process.env.PORT || 3000, function(){console.log("serving on localhost:3000")});
